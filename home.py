141294b94904b0211063d6ff8b85aad040048d60
Itamar Dalal <125656657+Itamar-Dalal@users.noreply.github.com>
2026-03-01 14:31:15 +0200

    Update home.py

--- home.py
+++ home.py
@@ -2,9 +2,10 @@ from scapy.all import *
 
 IFACE = r"\Device\NPF_{BBFAF8BF-8879-4C10-83D4-705B892AA256}"
 ENP0S8_MAC = "08:00:27:61:cb:8f"
+ENP0S9_IP = "192.168.2.1"
 
 def main():
-    pkt = Ether(dst=ENP0S8_MAC)/IP(dst="192.168.2.1")/ICMP()
+    pkt = Ether(dst=ENP0S8_MAC)/IP(dst=ENP0S9_IP)/ICMP()
     pkt.show()
     sendp(pkt, iface=IFACE)
 
