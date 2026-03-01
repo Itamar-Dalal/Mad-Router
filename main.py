b6d065d21097bee84b502b8181c33c2050bd218d
Itamar-Dalal <dalalitamar@gmail.com>
2026-03-01 16:48:09 +0200

    proxy

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
 from scapy.all import *
 
+
 IFACE_1 = "enp0s8"
 SUBNET_1 = "192.168.1"
 
 IFACE_2 = "enp0s9"
 SUBNET_2 = "192.168.2"
 
+PROXY_IP = "192.168.2.2"
+ALICE_IP = "192.168.1.1"
+
+routing_table = {} # subnet -> iface
+routing_table[SUBNET_1] = IFACE_1
+routing_table[SUBNET_2] = IFACE_2
+
+
+def modify_packet(pkt):
+    # check if Alice is not connecting to proxy
+    # if not, then change the src ip to proxy's ip
+    if pkt.sniffed_on == IFACE_1 and pkt[IP].dst != PROXY_IP:
+        pkt[IP].src = PROXY_IP
+
+    # change the dst ip back to alice's ip
+    if pkt.sniffed_on == IFACE_2 and pkt[IP].dst == PROXY_IP:
+        pkt[IP].dst = ALICE_IP
+
+    return pkt
+
+
+def route(pkt):
+    # modify proxy changes before route
+    pkt = modify_packet(pkt)
+    
+    for subnet in routing_table.keys():
+        if subnet in pkt[IP].dst:
+            # get only ingoing packets
+            if pkt.sniffed_on != routing_table[subnet]:
+                sendp(pkt, routing_table[subnet])
+
 
 def main():
-    while True:
-        sniff(iface=IFACE_1, prn=lambda pkt: sendp(pkt, IFACE_2), filter=f"ip dst {SUBNET_2}", count=1)
-        sniff(iface=IFACE_2, prn=lambda pkt: sendp(pkt, IFACE_1), filter=f"ip dst {SUBNET_1}", count=1)
+    sniff(iface=[IFACE_1, IFACE_2], prn=route, filter="ip")
 
 
 if __name__ == "__main__":
