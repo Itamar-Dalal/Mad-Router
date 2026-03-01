2ef0012e58c4c740bb61cb18b3a77d3acabb7462
Itamar-Dalal <dalalitamar@gmail.com>
2026-03-01 14:30:11 +0200

    finished

--- home.py
+++ home.py
@@ -1,13 +1,12 @@
 from scapy.all import *
 
-IFACE = "Ethernet 5"
-SUBNET = "192.168.1"
-
+IFACE = r"\Device\NPF_{BBFAF8BF-8879-4C10-83D4-705B892AA256}"
+ENP0S8_MAC = "08:00:27:61:cb:8f"
 
 def main():
-    pkt = IP(dst="192.168.2.2")
+    pkt = Ether(dst=ENP0S8_MAC)/IP(dst="192.168.2.1")/ICMP()
     pkt.show()
-    sendp(pkt, iface=IFACE, count=1)
+    sendp(pkt, iface=IFACE)
 
 
 if __name__ == "__main__":
